447486addbbd55ee91aa41ff400b1f1dd07f1309
Yaniv Kapilianov <yanivprojects@gmail.com>
2020-09-30 12:38:04 +0300

    Parameterization of ImageMediaCard.

--- web/src/views/Components/Sections/Home.js
+++ web/src/views/Components/Sections/Home.js
@@ -1,18 +1,8 @@
 import React from "react";
-// plugin that creates slider
-import Slider from "nouislider";
+
 // @material-ui/core components
 import { makeStyles } from "@material-ui/core/styles";
-import InputAdornment from "@material-ui/core/InputAdornment";
-import FormControlLabel from "@material-ui/core/FormControlLabel";
-import Checkbox from "@material-ui/core/Checkbox";
-import Radio from "@material-ui/core/Radio";
-import Switch from "@material-ui/core/Switch";
-// @material-ui/icons
-import Favorite from "@material-ui/icons/Favorite";
-import People from "@material-ui/icons/People";
-import Check from "@material-ui/icons/Check";
-import FiberManualRecord from "@material-ui/icons/FiberManualRecord";
+
 // core components
 import GridContainer from "components/Grid/GridContainer.js";
 import GridItem from "components/Grid/GridItem.js";
@@ -39,7 +29,7 @@ export default function Home() {
           </div>
           <GridContainer justify="center">
             <GridItem xs={6} sm={3}>
-                <ImgMediaCard></ImgMediaCard>
+                <ImgMediaCard headline="כדורעף בגנצו" description="כדורעף הכי שווה בבסיס עושים מלא מלא כיף!!"></ImgMediaCard>
             </GridItem>
             <GridItem xs={6} sm={3}>
                 <ImgMediaCard></ImgMediaCard>
